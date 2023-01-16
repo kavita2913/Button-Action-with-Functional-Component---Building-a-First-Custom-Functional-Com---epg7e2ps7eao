c815dd13908215c986bd11d7d517626081c23f67
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-17 00:05:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  function ButtonClick(){
+  const ButtonClick =() =>{
     <p id = "para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-  
-  }
+     }
   return (
     <div id="main">
-
-    <button id= "click" onClick={ButtonClick()}> Click</button>
+    <button id= "click" onClick={ButtonClick}> Click</button>
     </div>
   );
 }
