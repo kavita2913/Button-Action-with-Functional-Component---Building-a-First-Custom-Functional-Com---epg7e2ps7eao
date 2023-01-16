31a1fb353def07ebba055d22d0074aa819e2e780
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-16 14:05:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div id="main">
 
-    <button id= "click" onClick="click()">Click
+    <button id= "click" onClick="{click}">Click
     <p id = "para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </button>
     </div>
