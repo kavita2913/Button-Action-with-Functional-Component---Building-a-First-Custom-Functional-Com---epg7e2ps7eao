fe3c737bc18e0ef5529bbf012511114dad6c3aed
kavi <116868545+kavita2913@users.noreply.github.com>
2023-01-18 00:29:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const ButtonClick =() =>{
-   
-     }
+  const [hide, sethide] = useState(false);
+  const handleClick = () => {
+    const newHide = !hide;
+    sethide(newHide);
+  };
+
   return (
     <div id="main">
-    <button id= "click" onClick={ButtonClick}>click
-     <p id = "para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    </button>
+      <button id="click" onClick={handleClick}>
+        button
+      </button>
+      {hide === true ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 
 export default App;
